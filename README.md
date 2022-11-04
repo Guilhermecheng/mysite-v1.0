b4b3fae700acccc1d61406697c1821678f9c812b
Guilhermecheng <guilherme.cheng@gmail.com>
2022-11-04 20:01:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mysite-v1.0
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mysyte1)
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="">
+      <div className="w-full h-[60px] shadow-lg flex flex-col items-center justify-center">
+        <h1 className="text-gray-700"> Hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
